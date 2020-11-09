ea7f6c89af7c1f4099ad6193a068f424baf0ea69
Brad Runnels <runnels.brad@gmail.com>
2020-11-09 16:28:31 -0500

    [app/config/bootstrap.php] define constants for required directories

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -2,12 +2,16 @@
 
 date_default_timezone_set('America/New_York');
 
-define('APP_ROOT', dirname(__DIR__));
+$appDirectory = dirname(__DIR__);
+
 define('DS', DIRECTORY_SEPARATOR);
+define('DIR_COMPLETE', $appDirectory . DS . 'complete');
+define('DIR_ORDERS', $appDirectory . DS . 'orders');
+define('DIR_ZIP', $appDirectory . DS . 'zip');
 
 
-require dirname(__DIR__, 2) . '/vendor/autoload.php';
-$settings = (require 'settings.php');
+require dirname(__DIR__, 2) . DS . 'vendor' . DS . 'autoload.php';
+$settings = (require __DIR__ . DS . 'settings.php');
 
 
 return new Ocozzio\OrderAutomation\Controller\AppController();
