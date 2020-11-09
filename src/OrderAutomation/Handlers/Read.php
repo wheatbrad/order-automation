ab7fa3fc63d452e4e865408e1be4d08dc1426977
Brad Runnels <runnels.brad@gmail.com>
2020-11-09 14:40:21 -0500

    [src/OrderAutomation/Handlers/Read.php] complete checkForNewOrders, add method checkForOrphanedPackages

--- src/OrderAutomation/Handlers/Read.php
+++ src/OrderAutomation/Handlers/Read.php
@@ -9,7 +9,28 @@ class Read
     public static Iterable $orderFiles;
 
 
-    public static function checkForOrders(String $path) : Bool {}
+    public static function checkForNewOrders(String $path) : Bool {
+        $files = [];
+        
+        foreach(new \DirectoryIterator($path) as $fileInfo) {
+            if ($fileInfo->getExtension() === 'xml') {
+                $files[] = $fileInfo->getPathname();
+            }
+        }
+
+        return (bool) $files;
+    }
+
+
+    public static function checkForOrphanedPackages(String $path) : Bool {
+        foreach (new \DirectoryIterator($path) as $fileInfo) {
+            if ($fileInfo->getExtension() === 'zip') {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 
     public static function readNewOrders() : Iterable {}
