fbce04afbea39b7706d9fad76de7bd8d47a09fb2
Brad Runnels <runnels.brad@gmail.com>
2020-11-08 19:42:08 -0500

    [app/config/bootstrap.php] add config and constants

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
+date_default_timezone_set('America/New_York');
+
+define('APP_ROOT', dirname(__DIR__));
+define('DS', DIRECTORY_SEPARATOR);
+
+
 require dirname(__DIR__, 2) . '/vendor/autoload.php';
 $settings = (require 'settings.php');
 
