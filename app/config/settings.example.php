1d886b7a46980bedada4c61120f9568246cd77c8
Brad Runnels <runnels.brad@gmail.com>
2020-11-08 19:33:00 -0500

    [app/config/settings.example.php] remove config and constants from settings

--- app/config/settings.example.php
+++ app/config/settings.example.php
@@ -1,11 +1,5 @@
 <?php
 
-date_default_timezone_set('America/New_York');
-
-define('APP_ROOT', dirname(__DIR__));
-define('DS', DIRECTORY_SEPARATOR);
-
-
 return [
     'smtp' => [
         'host' => 'smtp.example.com',
