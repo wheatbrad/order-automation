c18e02ecabfc04302b6bb98a6d53d3e879387e91
Brad Runnels <runnels.brad@gmail.com>
2020-11-09 21:59:09 -0500

    [app/index.php] use directory separator constant

--- app/index.php
+++ app/index.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $app = (require __DIR__.'/config/bootstrap.php');
+    $app = (require __DIR__.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'bootstrap.php');
 
     if ($app->foundOrders()) {
         $app->processOrders();
