5b95bf8372a1017b5f5162156ae98aaee0d0dca4
Brad Runnels <runnels.brad@gmail.com>
2020-11-16 12:40:39 -0500

    [src/OrderAutomation/Handlers/Process.php] complete method removeDirectory

--- src/OrderAutomation/Handlers/Process.php
+++ src/OrderAutomation/Handlers/Process.php
@@ -2,6 +2,11 @@
 
 namespace Ocozzio\OrderAutomation\Handlers;
 
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SimpleXMLElement;
+use ZipArchive;
+
 
 class Process
 {
@@ -13,12 +18,25 @@ class Process
     }
 
 
-    private static function packageOrder(iterable $order) : void {}
+    private static function packageOrder(SimpleXMLElement $order) : void {}
 
 
     private static function createReceipt(iterable $order) : iterable {}
 
 
-    private static function removeDirectory(string $path) : void {}
+    private static function removeDirectory(string $path) : void {
+        $directory = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS);
+        $iterator = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::CHILD_FIRST);
+
+        foreach($iterator as $info) {
+            if ($info->isDir()) {
+                rmdir($info->getPathname());
+            } else {
+                unlink($info->getPathname());
+            }
+        }
+
+        rmdir($path);
+    }
     
 }
