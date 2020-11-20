335e1a6a5afde87ebed86d1227f0b00b2e6231f2
Brad Runnels <runnels.brad@gmail.com>
2020-11-20 10:15:40 -0500

    [src/OrderAutomation/Handlers/Send.php] complete Send::sendOrders method

--- src/OrderAutomation/Handlers/Send.php
+++ src/OrderAutomation/Handlers/Send.php
@@ -2,10 +2,39 @@
 
 namespace Ocozzio\PFAutomation\Domain\OrderHandling;
 
+use DirectoryIterator;
+use RuntimeException;
+
 
 class Send
 {
 
-    public static function sendOrders() : Iterable {}
+    public static function sendOrders(iterable $settings) : void {
+        foreach (new DirectoryIterator(DIR_ZIP) as $fileInfo) {
+            if ($fileInfo->getExtension() === 'zip') {
+                if (!$connection = @ftp_connect($settings['ftp_host'])) {
+                    throw new RuntimeException(('Unable to connect to {vendor} FTP'));
+                }
+        
+                if (!@ftp_login($connection, $settings['ftp_user'], $settings['ftp_password'])) {
+                    throw new RuntimeException('Invalid credentials for {vendor} FTP connection');
+                }
+        
+                ftp_pasv($connection, true);
+        
+                if (!ftp_chdir($connection, $settings['ftp_path'])) {
+                    throw new RuntimeException('Unable to change path on {vendor} FTP');
+                }
+        
+                if (!@ftp_put($connection, $fileInfo->getBasename(), $fileInfo->getPathname(), FTP_BINARY)) {
+                    throw new RuntimeException('Unable to put {file} on {vendor} FTP');
+                }
+
+                rename($fileInfo->getPathname(), DIR_COMPLETE.$fileInfo->getBasename());
+
+                ftp_close(($connection));
+            }
+        }
+    }
     
 }
